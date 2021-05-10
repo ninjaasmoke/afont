e6d11df320fcab193d76df9f4e46bc75531188d2
Nithin Sai K J <nithins674@gmail.com>
2021-05-10 14:17:34 +0530

    auto dark mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withPwa = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+// const withPwa = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPwa({
-    pwa: {
-        dest: 'public',
-        runtimeCaching,
-    }
-})
+// module.exports = withPwa({
+//     pwa: {
+//         dest: 'public',
+//         runtimeCaching,
+//     }
+// })
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react'
 import { AppWrapper } from '../context/AppContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const toggle = document.querySelector('.toggle')
+    const h = new Date().getHours()
+    toggle.checked = h > 7 & h < 20
+  }, [])
   return (
     <AppWrapper>
-      <input id="toggle" className="toggle" type="checkbox" />
+      <input id="toggle" className="toggle" type="checkbox" title="Toggle Dark Mode" />
       <div className="background">
         <Component {...pageProps} />
       </div>
