8f2be3093c357251624053cd1927e1ef8026a8b5
Nithin Sai K J <nithins674@gmail.com>
2021-05-10 14:33:59 +0530

    check pwa beta

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// const withPwa = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-// module.exports = withPwa({
-//     pwa: {
-//         dest: 'public',
-//         runtimeCaching,
-//     }
-// })
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
+})
