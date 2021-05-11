07ad2130685ddb309eef1565fa8bca2cf431b27c
Nithin Sai K J <nithins674@gmail.com>
2021-05-11 17:16:12 +0530

    html & css for instructions

--- pages/new.js
+++ pages/new.js
@@ -1,28 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Nav from '../components/nav'
 import styles from '../styles/New.module.css'
 
-import StepProgressBar from 'react-step-progress';
-import 'react-step-progress/dist/index.css';
-
-const step1Content = <h1>Step 1</h1>;
-const step2Content = <h1>Step 2</h1>;
-const step3Content = <h1>Step 3</h1>;
-const step4Content = <h1>Step 4</h1>;
-
-function step2Validator() {
-    return false;
-}
-
-function step3Validator() {
-    return true;
-}
-
 export default function New() {
-    const onSubmit = () => {
-        alert('submit data');
-    };
-
     return (
         <div className={styles.container}>
             <Head>
@@ -31,32 +12,32 @@ export default function New() {
 
             <Nav linkTo="/" navTitle="Create" />
 
-            {/* <div className={styles.progress}>
-                <StepProgressBar
-                    startingStep={0}
-                    // wrapperClass="progress-wrapper-custom"
-                    onSubmit={onSubmit}
-                    submitBtnName="Finish"
-                    previousBtnName="Back"
-                    nextBtnName="Next"
-                    steps={[
-                        {
-                            content: step1Content
-                        },
-                        {
-                            content: step2Content,
-                            validator: step2Validator
-                        },
-                        {
-                            content: step3Content,
-                            validator: step3Validator
-                        },
-                        {
-                            content: step4Content
-                        }
-                    ]}
-                />
-            </div> */}
+            <div className={styles.instructions}>
+
+                <div className={styles.step + ' ' + styles.active}>
+                    <h2>Step 1</h2>
+                    <p className={styles.stepInstr}>Pick a font.</p>
+                </div>
+
+                <div className={styles.step}>
+                    <h2>Step 2</h2>
+                    <p className={styles.stepInstr}>Edit your font.</p>
+                </div>
+
+                <div className={styles.step}>
+                    <h2>Step 3</h2>
+                    <p className={styles.stepInstr}>Copy the text required, and paste.</p>
+                </div>
+
+                <div className={styles.step}>
+                    <h2>Step 4</h2>
+                    <p className={styles.stepInstr}>Download.</p>
+                </div>
+            </div>
+
+            <div className={styles.gotoLearn}>
+                If you are new and not sure how to use this app, please visit{' '} <Link href="/learn"><a>Learn</a></Link>
+            </div>
         </div>
     )
 }
