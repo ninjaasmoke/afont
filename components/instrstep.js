c7f2ca707618bfe8dff6e711927133f6abbffba3
Nithin Sai K J <nithins674@gmail.com>
2021-05-12 11:24:42 +0530

    small anim for steps

--- components/instrstep.js
+++ components/instrstep.js
@@ -1,25 +1,33 @@
 import utils from './instrstep.module.css'
 
 export default function InstrStep({ steps }) {
+
+    const getClass = (step) => {
+        let c = steps - step
+        if (c > 0) return utils.step + ' ' + utils.done
+        if (c == 0) return utils.step + ' ' + utils.active
+        return utils.step
+    }
+
     return (
         <div className={utils.instructions}>
 
-            <div className={steps - 0 > 0 ? utils.step + ' ' + utils.active : utils.step}>
+            <div className={getClass(1)}>
                 <h2>Step 1</h2>
                 <p className={utils.stepInstr}>Pick a font.</p>
             </div>
 
-            <div className={steps - 1 > 0 ? utils.step + ' ' + utils.active : utils.step}>
+            <div className={getClass(2)}>
                 <h2>Step 2</h2>
                 <p className={utils.stepInstr}>Edit your font.</p>
             </div>
 
-            <div className={steps - 2 > 0 ? utils.step + ' ' + utils.active : utils.step}>
+            <div className={getClass(3)}>
                 <h2>Step 3</h2>
                 <p className={utils.stepInstr}>Copy the text required, and paste.</p>
             </div>
 
-            <div className={steps - 3 > 0 ? utils.step + ' ' + utils.active : utils.step}>
+            <div className={getClass(4)}>
                 <h2>Step 4</h2>
                 <p className={utils.stepInstr}>Download.</p>
             </div>
