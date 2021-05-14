fd9203f0ad82478548b144b45ca6863e8285f194
Nithin Sai K J <nithins674@gmail.com>
2021-05-15 00:10:31 +0530

    floored progress

--- components/loading.js
+++ components/loading.js
@@ -1,6 +1,7 @@
 import loadStyle from './loading.module.css'
 
 export default function Loading({ radius, stroke, progress }) {
+    progress = Math.floor(progress)
     const normalizedRadius = radius - stroke * 2;
     const circumference = normalizedRadius * 2 * Math.PI;
     const strokeDashoffset = circumference - progress / 100 * circumference;
