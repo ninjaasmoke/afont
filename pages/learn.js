9104751500fcf638b3562993fb1d1c8ac28b6eae
Nithin Sai K J <nithins674@gmail.com>
2021-05-17 00:57:12 +0530

    started learn js

--- pages/learn.js
+++ pages/learn.js
@@ -1,15 +1,24 @@
 import Head from 'next/head'
 import Nav from '../components/nav'
-import styles from '../styles/New.module.css'
+import learnStyles from '../styles/Learn.module.css'
 
 export default function Learn() {
     return (
-        <div className={styles.container}>
+        <div className={learnStyles.container}>
             <Head>
                 <title>Assignmentium | Learn</title>
             </Head>
 
             <Nav linkTo="/" navTitle="Learn" />
+
+            <h1 className={learnStyles.heading}>
+                1. How to use Assignmentium
+            </h1>
+
+            <div className={learnStyles.grid}>
+
+            </div>
+
         </div>
     )
 }
